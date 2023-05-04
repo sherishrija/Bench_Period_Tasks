9dac68a43eec3c77d1ee811bb7bcb96f7e972018
sherishrija <114993782+sherishrija@users.noreply.github.com>
2023-05-04 13:47:27 +0530

    Add files via upload

---
+++ 27-04-2023/5-UniqueCharinString.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codes
+{
+    class UniqueCharinString
+    {
+        static void Main()
+        {
+
+
+            string input;
+            Console.WriteLine("Enter the string");
+            input = Console.ReadLine();
+            char uniqueChar = '\0';
+            foreach (char c in input)
+            {
+                if (input.IndexOf(c) == input.LastIndexOf(c))
+                {
+                    uniqueChar = c;
+                    break;
+                }
+            }
+            if (uniqueChar == '\0')
+            {
+                Console.WriteLine("No unique character found");
+            }
+            else
+            {
+                Console.WriteLine("The unique character is:" + uniqueChar);
+            }
+        }
+
+    
+    }
+}
---
+++ 27-04-2023/8-vowels_consonants.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codes
+{
+    class vowels_consonants
+    {
+        public static void Main()
+        {
+            //string str = "Hello World";
+            string str;
+            Console.WriteLine("enter the string:");
+            str = Console.ReadLine();
+            int vowelCount = 0;
+            int consonantCount = 0;
+
+            foreach (char c in str.ToLower())
+            {
+                if (c >= 'a' && c <= 'z')
+                {
+                    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                    {
+                        vowelCount++;
+                    }
+                    else
+                    {
+                        consonantCount++;
+                    }
+                }
+            }
+
+            Console.WriteLine("Number of vowels: " + vowelCount);
+            Console.WriteLine("Number of consonants: " + consonantCount);
+        }
+    }
+}
+
